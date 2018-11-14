04447eee081b132daf790cbbb383b8a8b3641edf
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2018-11-14 00:59:39 -0200

    WIP barba.js

--- src/js/global.js
+++ src/js/global.js
@@ -1,8 +1,11 @@
-var viewport = window.innerHeight
-
-var introTitle = document.querySelector('#title')
-var introSocial = document.querySelector('.social')
-var introSocialBottom = introSocial.offsetTop + introSocial.offsetHeight
+// Sticky social links
+['scroll', 'touchstart', 'load', 'resize'].forEach(function(event) {
+  // var viewport = window.innerHeight
+  // var introTitle = document.querySelector('#title')
+  // var introSocial = document.querySelector('.social')
+  // var introSocialBottom = introSocial.offsetTop + introSocial.offsetHeight
+  // document.addEventListener(event, stickySocial, false)
+})
 
 function stickySocial(){
   var scroll = window.scrollY
@@ -17,6 +20,36 @@ function stickySocial(){
   }
 }
 
-['scroll', 'touchstart', 'load', 'resize'].forEach(function(event) {
-  document.addEventListener(event, stickySocial, false)
+// barba.js configs
+document.addEventListener('DOMContentLoaded', function() {
+  var Transition = Barba.BaseTransition.extend({
+    start: function() {
+      // this.newContainerLoading.then(this.heyholetsgo.bind(this))
+      // document.querySelector('#title').classList.add('teste')
+      // document.querySelector('#title').classList.add('teste')
+      console.log('start')
+
+      Promise
+        .all([this.newContainerLoading, this.fadeOut()])
+        .then(this.heyholetsgo.bind(this));
+    },
+
+    fadeOut: function() {
+      /**
+       * this.oldContainer is the HTMLElement of the old Container
+       */
+
+      return this.oldContainer.classList.add('teste')
+    },
+
+    heyholetsgo: function() {
+      this.done()
+      console.log('heyholetsgo')
+    }
+  })
+
+  Barba.Pjax.start()
+  Barba.Pjax.getTransition = function() {
+    return Transition
+  }
 })
