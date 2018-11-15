31d9b2a1b9fe0e93958f07702afc70fc3d8d90d7
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2018-11-15 11:54:21 -0200

    fix stickySocial only in intro

--- src/js/global.js
+++ src/js/global.js
@@ -1,24 +1,33 @@
+var bodyIntro = document.body.classList.contains('body-intro')
+
 // Sticky social links
-['scroll', 'touchstart', 'load', 'resize'].forEach(function(event) {
-  // var viewport = window.innerHeight
-  // var introTitle = document.querySelector('#title')
-  // var introSocial = document.querySelector('.social')
-  // var introSocialBottom = introSocial.offsetTop + introSocial.offsetHeight
-  // document.addEventListener(event, stickySocial, false)
-})
+if (bodyIntro) {
+  var viewport = window.innerHeight
+  var introTitle = document.querySelector('#title')
+  var introSocial = document.querySelector('#social')
+  var introSocialBottom = introSocial.offsetTop + introSocial.offsetHeight
+}
 
 function stickySocial(){
-  var scroll = window.scrollY
+  if (bodyIntro) {
+    var scroll = window.scrollY
 
-  var introTitleBottom = introTitle.offsetTop + introTitle.offsetHeight
-  var scrollBottom = (scroll + viewport) - (viewport - introSocialBottom)
+    var introTitleBottom = introTitle.offsetTop + introTitle.offsetHeight
+    var scrollBottom = (scroll + viewport) - (viewport - introSocialBottom)
 
-  if (scrollBottom > introTitleBottom) {
-    introSocial.classList.add('sticky')
-  } else {
-    introSocial.classList.remove('sticky')
+    if (scrollBottom > introTitleBottom) {
+      introSocial.classList.add('sticky')
+    } else {
+      introSocial.classList.remove('sticky')
+    }
   }
 }
+
+['scroll', 'touchstart', 'load', 'resize'].forEach(function(event) {
+  document.addEventListener(event, stickySocial, false)
+})
+
+
 
 // barba.js configs
 document.addEventListener('DOMContentLoaded', function() {
