a39c3768b79770da8f42be79a19cce109bae13a1
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2018-11-16 11:29:43 -0200

    ditching JS sticky

--- src/js/global.js
+++ src/js/global.js
@@ -1,41 +1,8 @@
-var bodyIntro = document.body.classList.contains('body-intro')
-
-// Sticky social links
-if (bodyIntro) {
-  var viewport = window.innerHeight
-  var introTitle = document.querySelector('#title')
-  var introSocial = document.querySelector('#social')
-  var introSocialBottom = introSocial.offsetTop + introSocial.offsetHeight
-}
-
-function stickySocial(){
-  if (bodyIntro) {
-    var scroll = window.scrollY
-
-    var introTitleBottom = introTitle.offsetTop + introTitle.offsetHeight
-    var scrollBottom = (scroll + viewport) - (viewport - introSocialBottom)
-
-    if (scrollBottom > introTitleBottom) {
-      introSocial.classList.add('sticky')
-    } else {
-      introSocial.classList.remove('sticky')
-    }
-  }
-}
-
-['scroll', 'touchstart', 'load', 'resize'].forEach(function(event) {
-  document.addEventListener(event, stickySocial, false)
-})
-
-
-
 // barba.js configs
 document.addEventListener('DOMContentLoaded', function() {
   var Transition = Barba.BaseTransition.extend({
     start: function() {
       // this.newContainerLoading.then(this.heyholetsgo.bind(this))
-      // document.querySelector('#title').classList.add('teste')
-      // document.querySelector('#title').classList.add('teste')
       console.log('start')
 
       Promise
