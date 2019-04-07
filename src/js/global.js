ad1fa9c3bf27e886a1a7dec29b6118483dd32b43
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2019-04-07 11:58:57 -0300

    add location.hash verification; fix mobile

--- src/js/global.js
+++ src/js/global.js
@@ -416,26 +416,44 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // tabs
 window.addEventListener('load', function() {
-  // store tabs variable
-  var myTabs = document.querySelectorAll('.tab-item a');
+  var myTabs = document.querySelectorAll('.tab-item a')
+  var myContentPanes = document.querySelectorAll('.tab')
+  var hash = location.hash.replace('#','')
+
+  if (hash != '') {
+    for (var i = 0; i < myTabs.length; i++) {
+      myTabs[i].classList.remove('on')
+      myContentPanes[i].classList.remove('on')
+
+      if (hash === myTabs[i].getAttribute('href').replace('#','')) {
+        myTabs[i].classList.add('on')
+        myContentPanes[i].classList.add('on')
+      }
+    }
+  }
+
   function myTabClicks(tabClickEvent) {
     for (var i = 0; i < myTabs.length; i++) {
-      myTabs[i].classList.remove('on');
+      myTabs[i].classList.remove('on')
     }
-    var clickedTab = tabClickEvent.currentTarget;
-    clickedTab.classList.add('on');
-    tabClickEvent.preventDefault();
 
-    var myContentPanes = document.querySelectorAll('.tab');
+    var clickedTab = tabClickEvent.currentTarget
+    clickedTab.classList.add('on')
+    tabClickEvent.preventDefault()
+
+    // myContentPanes = document.querySelectorAll('.tab')
     for (i = 0; i < myContentPanes.length; i++) {
-      myContentPanes[i].classList.remove('on');
+      myContentPanes[i].classList.remove('on')
     }
-    var anchorReference = tabClickEvent.target;
-    var activePaneId = anchorReference.getAttribute('href');
-    var activePane = document.querySelector(activePaneId);
-    activePane.classList.add('on');
+
+    var anchorReference = tabClickEvent.target
+    var activePaneId = anchorReference.getAttribute('href')
+    var activePane = document.querySelector(activePaneId)
+    activePane.classList.add('on')
+    history.pushState(null, null, anchorReference.getAttribute('href'))
   }
+
   for (i = 0; i < myTabs.length; i++) {
     myTabs[i].addEventListener('click', myTabClicks)
   }
-});
+})
