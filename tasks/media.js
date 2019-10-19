1439b081804396a2a7eb85f6afb217fd654158fc
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2019-10-19 17:21:06 -0300

    more update tasks

--- tasks/media.js
+++ tasks/media.js
@@ -1,12 +1,11 @@
 var gulp = require('gulp')
 var plumber = require('gulp-plumber')
-var browserSync = require('browser-sync').create()
 
 module.exports = function(config) {
   gulp.task('media', function() {
     return gulp.src(config.media.src)
+      .pipe(plumber())
       .pipe(gulp.dest(config.media.dest))
-      .pipe(browserSync.stream())
       .pipe(plumber.stop())
   })
 }
