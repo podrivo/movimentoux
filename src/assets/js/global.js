46ceb8d7787454163adab0acc81998a902068d12
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2019-11-03 19:15:42 -0300

    fix redirect admin

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -2,3 +2,13 @@ var body = document.body
 var episode = body.getAttribute('data-page') === 'episode'
 var season = body.getAttribute('data-page') === 'season'
 var mqSmall = window.matchMedia('(min-width: 768px)').matches
+
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", function (user) {
+    if (!user) {
+      window.netlifyIdentity.on("login", function () {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
