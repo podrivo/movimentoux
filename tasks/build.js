405d09a0ba6f6153381c3e1029def5cb617ca20f
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2019-11-03 19:08:02 -0300

    add to build

--- tasks/build.js
+++ tasks/build.js
@@ -3,6 +3,6 @@ var runSequence = require('gulp4-run-sequence').use(gulp)
 
 module.exports = function() {
   gulp.task('build', function(callback) {
-    runSequence('clean', ['html', 'scripts:build', 'styles', 'media:build', 'fonts'], 'sitemap', 'files', callback)
+    runSequence('clean', ['html', 'scripts:build', 'styles', 'media:build', 'fonts'], 'sitemap', 'files', 'admin', callback)
   })
 }
