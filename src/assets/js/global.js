d4511cbdac69d70edb5f3eb01ab01cbe793aa051
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2019-11-08 17:17:27 -0300

    fix scroll-snap

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -1,6 +1,8 @@
 var body = document.body
-var episode = body.getAttribute('data-page') === 'episode'
-var season = body.getAttribute('data-page') === 'season'
+var html = document.body.parentNode
+
+var episode = html.getAttribute('data-page') === 'episode'
+var season = html.getAttribute('data-page') === 'season'
 var mqSmall = window.matchMedia('(min-width: 768px)').matches
 
 
