5c86bc0601e3d77e7c35ec3c66eba1bece8b8144
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2019-11-08 19:23:33 -0300

    commit history span separation header

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -6,37 +6,22 @@ var season = html.getAttribute('data-page') === 'season'
 var mqSmall = window.matchMedia('(min-width: 768px)').matches
 
 
-// var introHeader = []
-var introHeader = document.querySelector('.intro-section .header')
-var introHeaderTextContent = introHeader.textContent
-var introHeaderInnerHTML = introHeader.innerHTML
-// console.log(introHeader);
-var introText = introHeaderInnerHTML.split(' ')
-// console.log(introText);
-var introTextFiltered = introText.filter(function(el) {return el != ''})
-// console.log(introTextFiltered);
-var introTextLast = introTextFiltered.pop()
-// console.log(introTextLast);
-var introTextFirst = introTextFiltered.join(' ')
-
-introHeader.innerHTML = introTextFirst + '<span>' + introTextLast + '</span>'; //join first part and last part and put the required html for the last word
-console.log(introHeaderInnerHTML);
-
-
-
-// $('h2.title').html(function () {
-// introHeader.innerHTML(function() {
-  // separate the text by spaces
-// var introText = introHeader.join()
-// var introText = introHeader.split(' ')
-// console.log(introText, introText.clean(''));
-  // drop the last word and store it in a variable
-// var introTextLast = introText.pop()
-  // join the text back and if it has more than 1 word add the span tag
-  // to the last word
-  // return text.join(" ") + (text.length > 0 ? ' <span class="last">' + last + '</span>' : last);
-// })
-
-// console.log(introHeader.innerHTML);
-// console.log(introHeader.textContent);
-// console.log(introText, introTextLast);
+var introHeader = document.querySelectorAll('.intro-section .header')
+introHeader.forEach(function(header){
+  // console.log(header);
+  // var introHeaderTextContent = header.textContent
+  var introHeaderInnerHTML = header.innerHTML.replace(/<span[^>]*>[^>]*<\/span>/g, '')
+  // console.log(introHeaderInnerHTML);
+  var introText = introHeaderInnerHTML.split(' ')
+  // console.log(introText);
+  var introTextFiltered = introText.filter(function(el) {return el != ''})
+  // console.log(introTextFiltered);
+  var introTextLast = introTextFiltered.pop()
+  // console.log(introTextLast);
+  var introTextFirst = introTextFiltered.join(' ')
+  
+  var spanFirst = '<span>' + introTextFirst + '</span>'
+  var spanLast = '<span>' + introTextLast + '</span>'
+  
+  header.innerHTML = spanFirst + spanLast
+})
