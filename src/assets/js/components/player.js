84fdca53e7bed55887fa238059bd9d98ba3e8d44
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2019-11-09 09:30:05 -0300

    :wip:

--- src/assets/js/components/player.js
+++ src/assets/js/components/player.js
@@ -14,6 +14,8 @@ if (episode) {
   var forwards = document.querySelector('.forwards')
   var downloadLink = document.querySelector('.download')
 
+  var attrData = player.getAttribute('data-soundcloud')
+
   function formatTime(timeCurrent) {
     var seconds = parseInt(timeCurrent % 60)
     var minutes = parseInt((timeCurrent / 60) % 60)
@@ -24,39 +26,64 @@ if (episode) {
     return format
   }
 
-  var url = 'https://soundcloud.com/movimento-ux/' + player.getAttribute('data-soundcloud')
-  soundcloud.resolve(url, function (track) {
+  var storageTime = localStorage.getItem(attrData)
+  // alert(localStorage.getItem(attrData));
+
+  if (storageTime) {
+    audio.currentTime = storageTime
+  } else {
+    localStorage.setItem(attrData, audio.currentTime)
+    // alert(localStorage.getItem(attrData));
+  }
+
+  var url = 'https://soundcloud.com/movimento-ux/' + attrData
+  soundcloud.resolve(url, function(track) {
     timeDuration.innerHTML = formatTime(soundcloud.duration)
     soundcloud.play()
-    soundcloud.pause()
+    // soundcloud.pause()
+    var playPromise = soundcloud.play()
+
+    if (playPromise !== undefined) {
+      playPromise.then(function() {
+        if (mqSmall) {
+          soundcloud.pause()
+        }
+      }).catch(function(error){
+        console.log(error)
+      })
+    }
     downloadLink.href = track.download_url + '?client_id=' + KEY
 
     var timelinePercentage = (audio.currentTime * 100 / soundcloud.duration).toFixed(2)
     percentage.style.width = timelinePercentage + '%'
   })
 
-  soundcloud.on('timeupdate', function () {
+  soundcloud.on('timeupdate', function() {
     var timelinePercentage = (audio.currentTime * 100 / audio.duration).toFixed(2)
     percentage.style.width = timelinePercentage + '%'
 
     timeCurrent.innerHTML = formatTime(audio.currentTime)
-    localStorage.setItem(player.getAttribute('data-soundcloud'), audio.currentTime)
+    localStorage.setItem(attrData, audio.currentTime)
+    // alert(localStorage.getItem(attrData));
 
-    timeline.addEventListener('click', function (e) {
+    timeline.addEventListener('click', function(e) {
       var percent = e.offsetX / this.offsetWidth
       audio.currentTime = percent * audio.duration
     })
   })
 
-  var storageTime = localStorage.getItem(player.getAttribute('data-soundcloud'))
-  if (storageTime) {
-    audio.currentTime = storageTime
-  } else {
-    localStorage.setItem(player.getAttribute('data-soundcloud'), audio.currentTime)
-  }
+  // var storageTime = localStorage.getItem(attrData)
+  // alert(localStorage.getItem(attrData));
+
+  // if (storageTime) {
+  //   audio.currentTime = storageTime
+  // } else {
+  //   localStorage.setItem(attrData, audio.currentTime)
+  //   alert(localStorage.getItem(attrData));
+  // }
 
   var playButton = document.querySelector('.episode-player > .play')
-  playButton.addEventListener('click', function () {
+  playButton.addEventListener('click', function() {
     if (soundcloud.playing) {
       soundcloud.pause()
       this.classList.remove('-on')
@@ -66,19 +93,19 @@ if (episode) {
     }
   })
 
-  timeline.addEventListener('mousemove', function (event) {
+  timeline.addEventListener('mousemove', function(event) {
     var hoverPercentage = (event.offsetX * 100 / this.offsetWidth).toFixed(2)
     percentageMouse.style.width = hoverPercentage + '%'
     percentageMouse.style.opacity = '1'
   })
 
-  timeline.addEventListener('mouseleave', function (event) {
+  timeline.addEventListener('mouseleave', function() {
     setTimeout(function () {
       percentageMouse.style.opacity = '0'
     })
   })
 
-  volume.addEventListener('click', function (event) {
+  volume.addEventListener('click', function() {
     if (this.classList.contains('-off')) {
       soundcloud.setVolume(1)
       this.classList.remove('-off')
@@ -88,11 +115,11 @@ if (episode) {
     }
   })
 
-  forwards.addEventListener('click', function (event) {
+  forwards.addEventListener('click', function() {
     audio.currentTime = audio.currentTime + 15
   })
 
-  backwards.addEventListener('click', function (event) {
+  backwards.addEventListener('click', function() {
     audio.currentTime = audio.currentTime - 15
   })
 }
